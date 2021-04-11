6b610a53e3910ae12890119c5977cadbd18a9db3
Willy Sudiarto Raharjo <willysr@gmail.com>
2021-04-12 00:11:13 +0700

    initial commit
    Signed-off-by: Willy Sudiarto Raharjo <willysr@gmail.com>

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,79 @@
+var store = [{
+        "title": "Modules",
+        "excerpt":"This course contains the following modules: Motivation Course objectives AWS RoboMaker AWS RoboMaker architecture Assessment for Module 1 - Introduction to AWS RoboMaker AWS core services AWS services at run-time Assessment for Module 2 - Necessary AWS Services Setting up AWS RoboMaker AWS RoboMaker Demo Waveshare Jetbot on AWS RoboMaker...","categories": [],
+        "tags": [],
+        "url": "/modules/index.html",
+        "teaser": null
+      },{
+        "title": "Motivation",
+        "excerpt":"In robotics, typically software developers write and test their code on local machines that need to be configured, and eventually deploy the developed software on a robot. This process introduces bottlenecks in the development cycle, including management of local machines and scalability problems. Cloud services have been growing over the...","categories": [],
+        "tags": [],
+        "url": "/modules/0/motivation.html",
+        "teaser": null
+      },{
+        "title": "Course objectives",
+        "excerpt":"How are we able to run the simulations and enable robot software development in the cloud, as seen in the previous video? This course will give you the basics on AWS RoboMaker and its connected services, allowing you to have a cloud environment to build and develop a robotics system...","categories": [],
+        "tags": [],
+        "url": "/modules/0/objectives.html",
+        "teaser": null
+      },{
+        "title": "AWS RoboMaker",
+        "excerpt":"In this module, we will discover the main features of AWS RoboMaker which will allow you to understand overall how everything is set up. What is AWS RoboMaker? AWS RoboMaker is a cloud service that allows robotic software developers to develop and simulate in the cloud, and deploy applications to...","categories": [],
+        "tags": [],
+        "url": "/modules/1/robomaker.html",
+        "teaser": null
+      },{
+        "title": "AWS RoboMaker architecture",
+        "excerpt":"How is AWS RoboMaker structured and what is possible within it? The following figure shows its architecture. There are two main components from AWS that are important to understand the architecture: services that run within AWS cloud, e.g., the AWS RoboMakeer Development environment, the Simulation. storage available to store the...","categories": [],
+        "tags": [],
+        "url": "/modules/1/architecture.html",
+        "teaser": null
+      },{
+        "title": "Assessment for Module 1 - Introduction to AWS RoboMaker",
+        "excerpt":"Linux commands This assignment is to help you self-assess your understanding on AWS RoboMaker’s architecture. Read carefully each question. Once answered, you will receive feedback. Good luck! Q1 AWS RoboMaker integrates which of the following software and services to speed up the development of robotics application? Select all that apply:...","categories": [],
+        "tags": [],
+        "url": "/modules/1/assessment.html",
+        "teaser": null
+      },{
+        "title": "AWS core services",
+        "excerpt":"As we discovered what AWS RoboMaker is and the features it provides, let’s discover in this module what are the AWS services that are needed to configure AWS RoboMaker. We will find out in the following module how to set up an AWS RoboMaker environment. What is needed for AWS...","categories": [],
+        "tags": [],
+        "url": "/modules/2/aws.html",
+        "teaser": null
+      },{
+        "title": "AWS services at run-time",
+        "excerpt":"After setting up the core services, there are a number of services that are useful to know to fully exploit the potential from RoboMaker and AWS. Amazon CloudWatch Amazon CloudWatch is a service that allows for complete observability of the system, e.g., by providing tools for monitoring application and for...","categories": [],
+        "tags": [],
+        "url": "/modules/2/services.html",
+        "teaser": null
+      },{
+        "title": "Assessment for Module 2 - Necessary AWS Services",
+        "excerpt":"AWS Services This assignment is to help you self-assess your understanding on AWS Services. Read carefully each question. Once answered, you will receive feedback. Good luck! Q1 Match the pairs. 0. Identity and Access Management (IAM) 3. computing resources 1. Amazon S3 0. grant permission 2. AWS Cloud9 2. cloud-based...","categories": [],
+        "tags": [],
+        "url": "/modules/2/assessment.html",
+        "teaser": null
+      },{
+        "title": "Setting up AWS RoboMaker",
+        "excerpt":"Now that we know the different pieces that are needed for AWS RoboMaker, let’s learn the main steps on setting up an AWS RoboMaker environment. As you run through these steps, if you have a question you might find an answer in the RoboMaker Q&amp;A forum. The first step is...","categories": [],
+        "tags": [],
+        "url": "/modules/3/robomaker.html",
+        "teaser": null
+      },{
+        "title": "AWS RoboMaker Demo",
+        "excerpt":"With the AWS robot development configured, let’s ensure that we understand the main tools and see a sample example which will show the simulation running. In this unit, we will learn what the structure of the code should be and how to run the simulation. We will learn in the...","categories": [],
+        "tags": [],
+        "url": "/modules/3/robomaker-demo.html",
+        "teaser": null
+      },{
+        "title": "Waveshare Jetbot on AWS RoboMaker",
+        "excerpt":"With the basics on AWS RoboMaker, we can set up a ROS package containing the model and the tools for the Gazebo simulation with the Waveshare Jetbot, the robot used as reference in this curriculum. Open Cloud9 To get back to Cloud9, click on Development environment on the left side...","categories": [],
+        "tags": [],
+        "url": "/modules/3/jetbot.html",
+        "teaser": null
+      },{
+        "title": "Assessment for Module 3 - AWS RoboMaker Development Environment",
+        "excerpt":"AWS RoboMaker setup This assignment is to help you self-assess your understanding on AWS RoboMaker setup and run. Read carefully each question. Once answered, you will receive feedback. Good luck! Q1 To set up AWS RoboMaker, the required steps are: (select all that apply) * Set up S3 bucket. *...","categories": [],
+        "tags": [],
+        "url": "/modules/3/assessment.html",
+        "teaser": null
+      },]
